157b0c7e68d66e5a0c38f970909fc2fd3cb0b9f5
NachitoPerez <irperez@fi.uba.ar>
2023-12-06 18:53:10 +0100

    change fifo sqs for a standard one

--- aws-cloud/src/main/java/cloud/tp/Constants.java
+++ aws-cloud/src/main/java/cloud/tp/Constants.java
@@ -2,9 +2,10 @@ package cloud.tp;
 import software.amazon.awssdk.regions.Region;
 
 public class Constants {
-    public static final String QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/307384070528/storesDailySales.fifo";
+    public static final String QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/307384070528/storesDailySales";
     public static final Region REGION = Region.US_EAST_1;
     public static final String BUCKET_NAME = "cloudprojecttest";
+    public static final String SQS_GROUP_ID = "client-app-group";
     
     // Add more constants as needed
 }
